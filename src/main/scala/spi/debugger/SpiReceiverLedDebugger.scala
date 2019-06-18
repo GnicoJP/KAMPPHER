75b8c630e4b7956d3c3ef226d56cf9bad57b8792
Gnico <gnico.gplus@yandex.com>
2019-06-18 23:29:52 +0900

    [ADD] SpiReceiverLedDebugger

---
+++ src/main/scala/spi/debugger/SpiReceiverLedDebugger.scala
@@ -0,0 +1,56 @@
+package spi.debugger
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.withClock
+
+class SpiReceiverLedDebugger extends Module {
+    val io = IO(new Bundle{
+        val CommandReadFinished = Input(Bool())
+        val ArgumentReadFinished = Input(Bool())
+        val Reading = Input(Bool())
+        val ReadSuccess = Input(Bool())
+        val Command = Input(UInt(6.W))
+        val CommandArgument = Input(UInt(32.W))
+
+        val CMD0 = Output(Bool())
+        val CMD8 = Output(Bool())
+        val ACMD41 = Output(Bool())
+        val CMD16 = Output(Bool())
+    })
+
+    val after_apcommand = RegInit(false.B)
+
+    val cmd0 = RegInit(false.B)  
+    val cmd8 = RegInit(false.B)
+    val acmd41 = RegInit(false.B)
+    val cmd16 = RegInit(false.B)
+
+    io.CMD0 := cmd0
+    io.CMD8 := cmd8
+    io.ACMD41 := acmd41
+    io.CMD16 := cmd16
+
+    when(ReadSuccess === true.B) {
+        when(after_apcommand) {
+            when(Command === 41.U) {
+                acmd41 := true.B
+            }
+        }.otherwise {
+            switch(Command) {
+                is(0.U){
+                    cmd0 := true.B
+                }
+                is(8.U) {
+                    cmd8 := true.B
+                }
+                is(55.U) {
+                    after_apcommand := true.B
+                }
+                is(16.U) {
+                    cmd16 := true.B
+                }
+            }
+        }
+    }
+}
