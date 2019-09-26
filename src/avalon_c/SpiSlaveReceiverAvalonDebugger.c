5e0a0f910f4a64761e7333d80606edaf03c11b9a
Gnico <gnico.gplus@yandex.com>
2019-09-26 23:39:18 +0900

    [ADD]SpiSlaveReceiverAvalonDebugger

---
+++ src/avalon_c/SpiSlaveReceiverAvalonDebugger.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define AXI_BASE ( 0xC0000000)
+#define AXI_BASE_SPAN ( 0xFC000000 - 0xC0000000 )
+
+#define MY_IP_BASE ( /* Base Address */ )
+
+volatile sig_atomic_t isContinue = 0;
+
+void signal_handle(int sig) {
+  isContinue = 1;
+}
+
+void binout32(uint32_t v) {
+  uint32_t mask = 1 << 31;
+  do
+      putchar(v & mask? '1' : '0');
+  while (mask >>= 1);
+}
+
+void output(uint64_t value, uint64_t * counter) {
+    unsigned int CMD = value >> 32;
+    unsigned int args = value & 0xffffffff;
+    printf("CMD%u|", CMD);
+    switch(CMD) {
+        case 17: case 18: case 24: case 25:
+            printf("BLOCK ADDRESS:%x", args);
+            break;
+        case 16:
+            printf("BLOCK SIZE:%x", args);
+            break;
+        default:
+            binout32(value);
+            printf("\n");
+            break;
+    }
+    (*counter)++;
+}
+
+int main() {
+    void *virtual_base;
+    int32_t fd;
+
+    // map the address space for the LED registers into user space so we can interact with them.
+    // we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
+
+    if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+        printf( "ERROR: could not open \"/dev/mem\"...\n" );
+        return( 1 );
+    }
+
+    virtual_base = mmap( NULL, AXI_BASE_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, AXI_BASE );
+
+    if( virtual_base == MAP_FAILED ) {
+        printf( "ERROR: mmap() failed...\n" );
+        close( fd );
+        return( 1 );
+    }
+
+    signal(SIGINT, signal_handle);
+
+    uint64_t * core_addr = (uint64_t *)(virtual_base + (unsigned long)MY_IP_BASE);
+    uint64_t got_version = 1;
+    uint64_t version = 1;
+    uint64_t counter = 0;
+    while( !isContinue ) {
+        got_version = *core_addr;
+        if(version != got_version) {
+            if(version > got_version) {
+                for(int i = version; i < 128; ++i)
+                    output(*(core_addr + i), &counter);
+                version = 1;
+            }
+            for(int i = version; i < got_version; ++i)
+                output(*(core_addr + i), &counter);
+            version = got_version;
+        } else {
+            fflush(stdout);
+        }
+    } // while
+    
+    // clean up our memory mapping and exit
+    if( munmap( virtual_base, AXI_BASE_SPAN ) != 0 ) {
+        printf( "ERROR: munmap() failed...\n" );
+        close( fd );
+        return( 1 );
+    }
+
+    close( fd );
+    return( 0 );
+}
---
+++ src/main/resources/SpiSlaveReceiverAvalonDebugger.v
@@ -0,0 +1,50 @@
+module SpiSlaveReceiverAvalonDebugger(
+  input         clock,
+  input         reset,
+  input  [6:0]  io_Avalon_address,
+  input         io_Avalon_read,
+  output [63:0] io_Avalon_readdata,
+  input         io_Avalon_write,
+  input  [63:0] io_Avalon_writedata,
+  output        io_Avalon_waitrequest,
+  input  [5:0]  io_Command,
+  input  [31:0] io_CommandArgument,
+  input         io_ReadSuccess
+);
+reg [37:0] mem [0:63];
+reg waitrequest;
+reg [2:0] bufferchanged_history;
+
+wire [5:0] itr;
+wire [5:0] mem0_next_inner;
+wire [38:0] mem0_next;
+
+assign io_Avalon_readdata = {26'b0, mem[io_Avalon_address]};
+assign io_Avalon_waitrequest = waitrequest;
+
+assign itr = mem[0][6:0];
+assign mem0_next_inner = itr + 7'b1;
+assign mem0_next =  mem0_next_inner == 7'b0 ? 38'b1 : {31'b0, mem0_next_inner};
+
+always @(posedge(clock)) begin
+    if(reset) begin
+        mem[0] = 38'b1;
+        bufferchanged_history = 3'b111;
+        waitrequest = 1'b0;
+    end else begin
+        bufferchanged_history = {bufferchanged_history[1:0], io_BufferChanged};
+        if(bufferchanged_history == 3'b011) begin
+            $display("RAISE itr[%b]", itr);
+            waitrequest = 1'b1;
+            mem[itr] <= {io_Command, io_CommandArgument};
+            $display("RAISE itr[%b]", itr);
+            mem[0] <= mem0_next;
+            $display("RAISE itr[%b]", itr);
+        end else begin
+            waitrequest = 1'b0;
+            $display("FALL itr[%b]", itr);
+        end
+    end
+end
+
+endmodule
